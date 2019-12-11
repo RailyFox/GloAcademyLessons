96c990cf19c489a422b388a0b3d8debc5294a997
RailyFox <Dragolen@mail.ru>
2019-12-11 12:04:05 +0300

    feat: add hover effect to primary button and before/after elements in hero section

---
+++ .sublime-gulp-tmp.js
@@ -0,0 +1,28 @@
+const {src, dest, watch} = require("gulp");
+const browserSync = require('browser-sync').create();
+const sass = require("gulp-sass");
+
+// Static server
+function bs() {
+	serverSass();
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    watch("./*.html").on('change', browserSync.reload);
+    watch("./sass/**/*.sass", serverSass);
+    watch("./sass/**/*.scss", serverSass);
+    watch("./js/*.js").on('change', browserSync.reload);
+};
+
+function serverSass() {
+    return src("./sass/**/*.sass", "./sass/**/*.scss")
+        .pipe(sass())
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
+/**/;module.exports = gulp;
